19b1daf6f4916d7048af32ee0c1bdf25e43c4f33
Severus <r1244460xx@gmail.com>
2021-04-23 00:17:04 +0800

    server deploy待實作

--- simulator.cpp
+++ simulator.cpp
@@ -8,13 +8,44 @@ int main () {
     simulator.init(time);
     while(time>0) {
         time -= simulator.event();
-        //simulator.eval();
     }
     simulator.print();
 }
 /*----------Service class-------------*/
+
+Service::Service(int t) {
+    switch(t) {
+        case URLLC:
+            thuput = rand()%9951+50;
+            d_bw = 10000;
+            d_delay = 1.;
+            d_cpu = 1;
+            d_mem = 2;
+            type = t;
+            break;
+
+        case EMBB:
+            thuput = rand()%150001+50000;
+            d_bw = 200000;
+            d_delay = 100.;
+            d_cpu = 2;
+            d_mem = 4;
+            type = t;
+            break;
+
+        case MMTC:
+            thuput = rand()%101+1;
+            d_bw = 100;
+            d_delay = 1000.;
+            d_cpu = 1;
+            d_mem = 1;
+            type = t;
+            break;
+    }
+}
+
 bool Service::is_satified() {
-    if(e2e_delay<=d_delay) {
+    if(e2e_delay <= d_delay) {
         return true;
     }else {
         return false;
@@ -22,43 +53,63 @@ bool Service::is_satified() {
 }
 
 /*----------Server class-------------*/
-int Server::get_intfc() {
-    double k0;
-    double k1;
-    double k2;
-    return k0 + k1*d_cpu/s_cpu + k2*d_mem/s_mem;
+Server::Server(int t) {
+    switch(t) {
+        case MEC:
+            s_cpu = 16;
+            s_mem = 64;
+            cr = 3;
+            node_delay = 0.1;
+            break;
+        case CC:
+            s_cpu = 4;
+            s_mem = 16;
+            cr = 3;
+            node_delay = 0.1;
+            break;
+    }
 }
 
-bool Server::deploy(Service ser) {
-    if(d_cpu+ser.d_cpu<=s_cpu && d_mem+ser.d_mem<=s_mem) {
-        d_cpu += ser.d_cpu;
-        d_mem += ser.d_mem;
-        service_list.push_back(ser);
-        return true;
-    }else{
-        return false;
-    }
-    
+int Server::get_intfc() {
+
+}
+
+int Server::get_thuput(Service service) {
+    return service.thuput * intfc;
+}
+
+void Server::deploy(Service ser) {
     
 }
 
 /*----------Link class-------------*/
-bool Link::deploy(Service ser) {
-    if(d_bw+ser.thuput <= s_bw) {
-        d_bw += ser.thuput;
-        return true;
-    }else {
-        return false;
-    }
+void Link::deploy(Service ser) {
+
 }
 
 /*----------Simulator class-------------*/
-void Simulator::eval() {
-    
+int Simulator::event() {
+    int e = rand()%2;
+    if(e == ARRIVE) {
+        arrive();
+    }else if(e == DEPART) {
+        depart();
+    }
+    return rand()%10+1; //1~9 seconds
 }
 
-int Simulator::event() {
-    //arrive or depart 
+void Simulator::arrive() {
+    int s = rand()%3;
+    Service service(s);
+    service_set.push_back(s);
+    vector<Server>::iterator iter = Proposal::eval(service, mec_set, cc_set);
+    if(iter->avail(service)) {
+        iter->deploy(service);
+    }
+}
+
+void Simulator::depart() {
+    
 }
 
 void Simulator::init(int seed) {
@@ -74,4 +125,39 @@ void Metrics::print() {
     cout << "Accepted ratio: " << acc_ratio << endl;
     cout << "Average delay: " << avg_delay << endl;
     cout << "Normalized throughput: " << nor_thuput << endl;
+}
+
+/*----------Proposal class-------------*/
+vector<Server>::iterator Proposal::eval(Service service, vector<Server>& mec_set, vector<Server>& cc_set) {
+    int mec_score = 0;
+    int cc_score = 0;
+    vector<Server>::iterator mec_iter = mec_set.begin();
+    vector<Server>::iterator cc_iter = cc_set.begin();
+    int mec_index = -1;
+    int cc_index = -1;
+    for(int i=0; i<mec_set.size(); i++) {  //Choose the best MEC server
+        int score = get_score(service, mec_set[i]);
+        if(score > mec_score) {
+            mec_score = score;
+            mec_index = i;
+        }
+    }
+    mec_iter += mec_index;
+    for(int i=0; i<cc_set.size(); i++) { //Choose the best CC server
+        int score = get_score(service, cc_set[i]);
+        if(score > cc_score) {
+            cc_score = score;
+            cc_index = i;
+        }
+    }
+    cc_iter += cc_index;
+    if(mec_score > cc_score) { //Compare MEC and CC server
+        return mec_iter;
+    }else {
+        return cc_iter;
+    }
+}
+
+int Proposal::get_score(Service service, Server server) {
+
 }
--- simulator.h
+++ simulator.h
@@ -4,124 +4,108 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 class Service {
     public:
-        int d_cpu;
-        int d_mem;
-        int thuput;
-        int e2e_delay;
-        int d_delay;
-        int d_bw;
-        bool accepted;
-        int get_e2e_delay();
+        int type = 0;
+        int d_cpu = 0;
+        int d_mem = 0;
+        int thuput = 0;
+        double e2e_delay = 0.;
+        double d_delay = 0.;
+        int d_bw = 0;
+        bool accepted = false;
+        Service(int t);
+        double get_e2e_delay();
         bool is_satified();
 
 };
 
-class URLLC : public Service {
-    public:
-        URLLC();
-};
-
-class EMBB : public Service {
-    public:
-        EMBB();
-};
-
 class Server {
     public:
-        int d_mem;
-        int d_cpu;
-        int s_mem;
-        int s_cpu;
-        int intfc; //interference
-        int proc_delay;
-        vector<Service> service_list;
-        int get_intfc();
-        bool deploy(Service ser);
-};
-
-class MEC : public Server {
-    public:
-        MEC();
-};
-
-class CC: public Server {
-    public:
-        CC();
-};
-
-class Cluster  {
-    public:
-        vector<Server> server_list;
-        Link *front = NULL;
-        Link *rear = NULL;
-};
-
-class Edge: public Cluster {
-    public:
-        vector<MEC> mec_list;
-};
-
-class Core: public Cluster {
-    public:
-        vector<CC> cc_list;
+        int d_mem = 0;
+        int d_cpu = 0;
+        int s_mem = 0;
+        int s_cpu = 0;
+        int cr = 0; //concentration rate
+        int intfc = 0; //interference
+        double node_delay = 0.; //rsp delay
+        vector<Service> service_list; //deployed service
+        Server(int t);
+        int get_intfc(); //update intfc 
+        double get_node_delay(); //update node delay;
+        bool avail(Service ser); //Available for this deployment
+        int get_thuput(Service ser);  //get interferenced thuput
+        void deploy(Service ser); //exec the deployment
 };
 
 class Link {
     public:
-        int prop_delay;
-        int s_bw;
-        int d_bw;
-        Cluster *front = NULL;
-        Cluster *rear = NULL;
+        double link_delay = 0.;
+        int s_bw = 0;
+        int d_bw = 0;
+        int cr; //concentration rate
         Link();
-        bool deploy(Service ser);
+        double get_link_delay(); //update link delay
+        bool avail(Service ser); //Available for this deployment
+        void deploy(Service ser); //Execute the deployment
 };
 
 class Metrics {
     public:
-        int exec_time;
-        int acc_ratio;
-        int avg_delay;
-        int nor_thuput;
+        int exec_time = 0;
+        double acc_ratio = 0.;
+        double avg_delay = 0.;
+        int nor_thuput = 0;
         void print();
 };
 
 class Simulator {
     public:
-        vector<MEC> mec_set;
-        vector<CC> cc_set;
-        vector<Service> req_set;
-        vector<Link> link_set;
-        vector<Service> acc_set;
+        vector<Service> service_set;
+        vector<Server> mec_set;
+        vector<Server> cc_set;
         Link ue_mec;
-        Link mec_cc;
-        int sim_time;
+        Link ue_cc;
+        int sim_time = 0;
         Metrics mtr;
         Simulator();
-        void eval();
-        void deploy(Service service);
-        void arrive();
-        void depart();
-        void init(int seed);
-        int event();
-        void print();
+        void init(int seed); //init parameter
+        int event(); //randomly generate a event, return timestamp
+        void arrive();  //arrive event
+        void depart(); //depart event
+        void deploy(); //Execute the deployment
+        void print(); //Print metrics
 
 };
 
 class Proposal {
-    
+    public:
+    static vector<Server>::iterator eval(Service service, vector<Server>& mec_set, vector<Server>& cc_set); //evaluate between CC and MEC choice, return chosed one
+    static int get_score(Service service, Server server);
 };
 
 class AIA {
+    public:
+    vector<Server>::iterator eval(Service service, vector<Server>& mec_set, vector<Server>& cc_set); //evaluate between CC and MEC choice, return chosed one
 
 };
 
+enum SERVER {
+    MEC,
+    CC,
+};
 
+enum EVENT {
+    ARRIVE,
+    DEPART,
+};
 
-
-
+enum SERVICE {
+    URLLC,
+    EMBB,
+    MMTC,
+};
 
 
 
